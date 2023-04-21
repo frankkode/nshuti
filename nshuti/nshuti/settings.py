f7224bf55a8784ff4b4f612cbc1bafa2f428c468
masacode <masasimpafra@gmail.com>
2023-04-21 20:15:59 +0300

    add email handler

--- nshuti/nshuti/settings.py
+++ nshuti/nshuti/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = False
 
 ALLOWED_HOSTS = ['https://nshutih.com','https://www.nshutih.com','www.nshutih.com','nshutih.com','https://nshuti.up.railway.app/','localhost']
-
+CSRF_TRUSTED_ORIGINS = ['https://nshutih.com','https://www.nshutih.com','www.nshutih.com','nshutih.com','https://nshuti.up.railway.app/']
 # Application definition
 
 INSTALLED_APPS = [
